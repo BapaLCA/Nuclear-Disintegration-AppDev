3bbd16d56f01071bfa58fd347c654835edcfba76
me when <romain.bouche@etu.univ-grenoble-alpes.fr>
2024-06-10 10:41:01 +0200

    Frequency select
    Added an empty space to set the frequency used by PIC18F to setup the X axis on the graph

--- modules/fileManaging.py
+++ modules/fileManaging.py
@@ -2,7 +2,8 @@ import csv
 from collections import defaultdict
 from tkinter import filedialog
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from tkinter import filedialog
+from tkinter import Tk, Button
+import matplotlib.pyplot as plt
 
 # Définition globale pour être utilisée par d'autres fonctions
 data = defaultdict(list)
@@ -10,11 +11,12 @@ data = defaultdict(list)
 def exit():
     quit()  # Pour fermer l'application
 
-def open_file(canvas, ax):
+def open_file(canvas, ax, freq):
     filepath = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])  # Ouverture d'un explorateur de fichier pour sélectionner un fichier csv à lire
     if not filepath:
         return  # Termine la fonction ici si aucun fichier n'est sélectionné
-    data = defaultdict(list)
+    global data  # Utilise la variable globale data
+    data.clear()  # Efface les anciennes données
     with open(filepath, 'r') as csvfile:  # Ouverture du fichier spécifié en tant que csv
         csvreader = csv.reader(csvfile, delimiter=';')  # Définition du séparateur de données
         for row in csvreader:  # Lecture des données
@@ -29,18 +31,18 @@ def open_file(canvas, ax):
                 # Log the error and continue
                 print(f"Error processing line : {row}. IndexError: {e}")
                 continue
-    plot_data(data, canvas, ax)  # Appel de la fonction d'affichage des données
+    plot_data(data, canvas, ax, freq)  # Appel de la fonction d'affichage des données
     return data # On récupère les données dans la fonction qui l'a appelée
 
-def plot_data(data, canvas, ax):
+def plot_data(data, canvas, ax, freq):
     keys = list(data.keys())  # Échelle horizontale (canaux)
     values = [sum(data[key]) for key in keys]  # On somme les valeurs des canaux entre elles pour les combiner
     
-    # On adapte les canaux au bon temps (On suppose que le clock utilisée est de 20µs)
-    scaled_keys = [key * 20 for key in keys]
-    
+    # Transformation des canaux en temps (20 microsecondes par canal)
+    scaled_keys = [key * 1000000/freq for key in keys]
+
     # On filtre les valeurs pour savoir jusqu'où elles ne sont pas nulles
-    non_zero_keys = [key for key, value in zip(keys, values) if value != 0]
+    non_zero_keys = [key for key, value in zip(scaled_keys, values) if value != 0]
     
     if not non_zero_keys:
         return  # Si toutes les valeurs sont nulles, on ne trace rien
@@ -50,7 +52,7 @@ def plot_data(data, canvas, ax):
     x_max = max(non_zero_keys)
 
     ax.clear()  # Effacer le graphique précédent
-    ax.bar(keys, values)  # Tracer en graphique à barre des valeurs
+    ax.bar(scaled_keys, values, width=20)  # Tracer en graphique à barre des valeurs, avec une largeur adaptée
     ax.set_xlim(x_min, x_max)  # On utilise l'intervalle défini pour zoomer
     ax.set_title('Graph Displayer')
     ax.set_xlabel('Time (in µs)')
--- modules/fitFunctions.py
+++ modules/fitFunctions.py
@@ -7,65 +7,81 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
 
-def erlang_pdf(x, A, k, lambd): # Definition de la formule de la distribution d'Erlang
+
+def erlang_pdf(x, A, k, lambd):
+    """Définition de la formule de la distribution d'Erlang."""
     return A * (lambd ** k * x ** (k - 1) * np.exp(-lambd * x)) / math.factorial(k - 1)
 
-def add_erlang_fit(data, canvas, ax, selected_value): # Fonction pour l'affichage de l'exponentielle correspondant à la distribution d'Erlang
-    k_value = selected_value.get() # On récupère la valeur de k dans le menu déroulant
+def add_erlang_fit(data, canvas, ax, selected_value, freq):
+    """Fonction pour l'affichage de l'exponentielle correspondant à la distribution d'Erlang."""
+    k_value = selected_value.get()  # On récupère la valeur de k dans le menu déroulant
     keys = list(data.keys())
     values = [sum(data[key]) for key in keys]
     
-    # Appliquer le facteur x20 à l'échelle de temps
-    scaled_keys = [key * 1 for key in keys]
+    # Mise à l'échelle de l'axe des temps
+    scaled_keys = [key * 20 for key in keys]  # 20 microsecondes par canal
     
-    # Ajustement de la courbe d'Erlang aux données
-    popt, _ = curve_fit(lambda x, A, lambd: erlang_pdf(x, A, k_value, lambd), scaled_keys, values, p0=[max(values), 0.1])
+    try:
+        # Ajustement de la courbe d'Erlang aux données
+        popt, _ = curve_fit(lambda x, A, lambd: erlang_pdf(x, A, k_value, lambd), scaled_keys, values, p0=[max(values), 0.1])
+        
+        # Récupération des paramètres optimaux (lambd seulement, k est connu)
+        A_optimal, lambd_optimal = popt
+        
+        # Générer des points pour l'axe x
+        x_data = np.linspace(min(scaled_keys), max(scaled_keys), 1000)
+        
+        # Calculer les valeurs de la fonction d'Erlang
+        y_data = erlang_pdf(x_data, A_optimal, k_value, lambd_optimal)
+        
+        # Tracé des données expérimentales et de l'ajustement de la courbe d'Erlang
+        ax.plot(x_data, y_data, color='red', linewidth=2, label=f'Distribution d\'Erlang (k={k_value}, λ={lambd_optimal * 1000000:.2f}, A={A_optimal})')
+        ax.legend()
+    except RuntimeError as e:
+        print(f"Erreur d'ajustement pour k={k_value}: {e}")
+    except Exception as e:
+        print(f"Erreur inattendue: {e}")
     
-    # Récupération des paramètres optimaux (lambd seulement, k est connu)
-    A_optimal, lambd_optimal = popt
-    # Générer des points pour l'axe x
-    x_data = np.linspace(min(scaled_keys), max(scaled_keys), 1000)
-    
-    # Calculer les valeurs de la fonction d'Erlang
-    y_data = erlang_pdf(x_data, A_optimal, k_value, lambd_optimal)
-    
-    # Tracé des données expérimentales et de l'ajustement de la courbe d'Erlang
-    ax.plot(x_data, y_data, color='red', linewidth=2, label=f'Distribution d\'Erlang (k={k_value}, λ={lambd_optimal*50000:.2f}, A={A_optimal})')
-    ax.legend()
     canvas.draw()  # Redessiner le canvas
 
     
 def gauss(x, a, x0, sigma): #definition de la formule generale d'une Gausienne
     return a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
 
-def add_gaussian_fit(data, canvas, ax):
+def add_gaussian_fit(data, canvas, ax, freq):
     if not data:
         return
     
     keys = np.array(list(data.keys())) # Echelle horizontale (canaux)
     values = np.array([sum(data[key]) for key in keys]) # On somme les valeurs des canaux entre elles pour les combiner
 
-    mean = sum(keys * values) / sum(values)
-    sigma = np.sqrt(sum(values * (keys - mean) ** 2) / sum(values))
+    # Mise à l'échelle de l'axe des temps
+    scaled_keys = [key * 1000000/freq for key in keys]
 
-    popt, pcov = curve_fit(gauss, keys, values, p0=[max(values), mean, sigma])
+    mean = sum(scaled_keys * values) / sum(values)
+    sigma = np.sqrt(sum(values * (scaled_keys - mean) ** 2) / sum(values))
+
+    popt, pcov = curve_fit(gauss, scaled_keys, values, p0=[max(values), mean, sigma])
     
-    x_fit = np.linspace(min(keys), max(keys), 1000)
+    x_fit = np.linspace(min(scaled_keys), max(scaled_keys), 1000)
     y_fit = gauss(x_fit, *popt)
     
     ax.plot(x_fit, y_fit, color='green', label='Gaussienne ajustée')
     ax.legend()
     canvas.draw()
     
-def add_poisson_fit(data, canvas, ax):
+def add_poisson_fit(data, canvas, ax, freq):
     keys = np.array(list(data.keys())) # Echelle horizontale (canaux)
     values = np.array([sum(data[key]) for key in keys]) # On somme les valeurs des canaux entre elles pour les combiner
 
+    # Mise à l'échelle de l'axe des temps
+    scaled_keys = [key * 1000000/freq for key in keys]
+
     # Estimation de la moyenne de la distribution de Poisson
-    mean = sum(keys * values) / sum(values)
+    mean = sum(scaled_keys * values) / sum(values)
     
     # Génération des valeurs de la distribution de Poisson
-    poisson_dist = poisson.pmf(keys, mean) * sum(values)
+    poisson_dist = poisson.pmf(scaled_keys, mean) * sum(values)
     
     ax.plot(keys, poisson_dist, color='blue', label='Distribution de Poisson')
     ax.legend()
